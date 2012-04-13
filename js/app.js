d9f1d7b16bb3925dca6c8ff6d779a50b3227efcf
Wes Bangerter <wes@custombit.com>
2012-04-13 10:13:37 -0600

    Add basic ember app that displays popular instagram photos

---
+++ js/app.js
@@ -0,0 +1,22 @@
+Embergram = Ember.Application.create();
+
+Embergram.Image = Ember.Object.extend({
+  thumbnail_url: null
+});
+
+Embergram.imagesController = Ember.ArrayController.create({
+  content: [],
+  createImage: function(thumbnail_url) {
+    var image = Embergram.Image.create({ thumbnail_url: thumbnail_url });
+    this.pushObject(image);
+  }
+});
+
+$(function(){
+  // Load the popular images from instagram
+  $.get('https://api.instagram.com/v1/media/popular?client_id=49a422e3020c43259c55e1c59722cdd6', function(response) {
+    $.each(response.data, function(index, record){
+      Embergram.imagesController.createImage(record.images.thumbnail.url);
+    });
+  });
+});
